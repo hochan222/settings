0f7640eb5fe662c81c511934ff052c4790ec3e5a
hochan222 <hochan049@gmail.com>
2020-10-29 22:27:42 +0900

    [REFACTOR] files

--- zsh-settings.sh
+++ zsh-settings.sh
@@ -14,5 +14,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # vim ~/.zshrc
-sed -e s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g ~/.zshrc > temp.sh && cat temp.sh > ~/.zshrc && rm -rf temp.sh
+# sed -e s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k/powerlevel10k\"/g ~/.zshrc > temp.sh && cat temp.sh > ~/.zshrc && rm -rf temp.sh
 
